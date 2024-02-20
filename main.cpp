a540dbe47371dcffa4b1473b2a89be1e66e49e3e
Kirya <sekiprogram@mail.ru>
2024-02-20 11:14:40 +0300

    added mult

--- main.cpp
+++ main.cpp
@@ -1,9 +1,13 @@
 #include <iostream>
+
 #include "mylib.h"
 #include "printer.h"
+#include "mult.h"
 
-int main(){
+int main() {
 	int num = give_number();
-	print(num);	
+	int num_2 = give_number();
+	int mult_res = mult(num, num_2);
+	print(mult_res);
 	return 0;
 }
